9328d15a92876a0a1287fcd2aaa3cb4eb12cc39a
Faith <oyebodefaith3@gmail.com>
2021-08-05 01:50:14 +0100

    added logo image to the header

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Center, Flex } from '@chakra-ui/react'
+import { Center, Flex } from '@chakra-ui/react';
+import { StaticImage } from "gatsby-plugin-image";
+
 
 const TopBar = () => {
     return (
         <Flex direction="column" justify="center" align={{base: 'center', xl: 'flex-start'}} background="#292929" height={{base: '67px', md: '140px'}}>
             <Center h={{base: '33.58px', md: '60px'}} w={{base: '108px', md: '193px'}} ml={{base: null, xl: '77px'}} border="1px" borderStyle="solid" borderColor="#FFFFFF" fontFamily="serif" fontSize={{base: "14px", md: "24px"}} color="white">
-                MyTestApp
+                <StaticImage src="../assets/images/logo.svg" alt="movie app logo" />
             </Center>
         </Flex>
     )
