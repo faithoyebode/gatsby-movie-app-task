d4a7bf3908e04c2070b64148c7f14eb223f97b00
Faith <oyebodefaith3@gmail.com>
2021-08-05 04:36:53 +0100

    turned hero background image to gatsby static image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { StaticImage } from "gatsby-plugin-image";
 
 const Hero = () => {
     return (
         <div className="hero">
-            <h3>Watch something incredible.</h3>
+            <StaticImage
+                style={{
+                gridArea: "1/1",
+                objectFit: "cover",
+                }}
+                layout="fullWidth"
+                alt=""
+                src="../assets/images/hero-lg.jpg"
+                className="hero-bg"
+            />
+            <div
+                style={{
+                    gridArea: "1/1",
+                    position: "relative",
+                }}
+                className="text-container"
+            >
+                <h3>Watch something incredible.</h3>
+            </div>
         </div>
     )
 }
